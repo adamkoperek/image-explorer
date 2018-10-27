54722eb8eca8eaed8db27e9c905e9a6ba3c13a62
adamkoperek <43070114+adamkoperek@users.noreply.github.com>
2018-10-27 12:48:18 +0200

    image magick path

--- back-end/config/environments/development.rb
+++ back-end/config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.image_magick_path = 'E:\\projects\\self\\image-explorer\\usefult-stuff\\image-magick-portable\\'
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
